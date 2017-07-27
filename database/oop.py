7a7e2877b61dfc42f0312e01ca12c64d8ffa9f1d
isaactzh <isaactzh0102@gmail.com>
2017-07-27 00:54:13 -0700

    end

--- database/oop.py
+++ database/oop.py
@@ -18,3 +18,36 @@ an.party()
 #construtor and destructor
 #the construtor and destructor are optional, The constructor is typically used
 #to set up variables and the destructor is seldom used.
+class PartyAniaml:
+    x = 0
+    name = ""
+    def __init__(self, z):
+        self.name = z
+        print(self.name, "constructed")
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, "party count", self.x)
+s = PartyAniaml("Sally")
+s.party()
+
+j = PartyAniaml("Jim")
+j.party()
+s.party()
+
+#Object inheritance
+#When we make a new class- We can reuse an existing class and inherit all
+#thed capabilities of an existing class and then add our own little bit to make
+#our new class
+#subclasses
+
+
+class FootballFan(PartyAniaml):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, "points", self.points)
+
+j = FootballFan("Jim")
+j.party()
+j.touchdown()
